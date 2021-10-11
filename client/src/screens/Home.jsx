a0b8ed722ba9ff0ffbe1004bbd873e4f3e54ba4f
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-11 09:18:41 -0400

    Styling touch ups

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -4,12 +4,11 @@ import Layout from "../components/Layout";
 function Home() {
   return (
     <Layout>
-      <div>
-        <h1>Welcome</h1>
-        <p className="musicphileDescription">Musicphile is a music sharing library. Share what you find with the world, and amuse yourself with arcane tunes brought to you by other music enthusiasts.</p>
+      <div className='homeContainer'>
+        <h1 className='welcome'>Welcome</h1>
+        <p className="musicphileDescription">Musicphile is a music sharing library. Share what you find with the world, and amuse yourself with arcane tunes brought to you by other music enthusiasts. 🎶</p>
       </div>
       <div>
-        <p>🎶</p>
       </div>
     </Layout>
   )
